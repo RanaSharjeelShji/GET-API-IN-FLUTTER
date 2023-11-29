46886d7e4dc1b9f4f3d7881a09a8e80f6450cb79
sharjeelshji <89970169+RanaSharjeelShji@users.noreply.github.com>
2023-11-30 02:46:01 +0500

    s

--- lib/home.dart
+++ lib/home.dart
@@ -1,24 +1,20 @@
 import 'dart:convert';
 
 import 'package:episode_api/constant_url.dart';
-import 'package:episode_api/todo_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
+//Mode Specific Function for get API is at the end of this file
 var todo = [];
 Future<List<dynamic>> getDataFromApi(String apiUrl) async {
   try {
     var url = Uri.parse(apiUrl);
     var response = await http.get(url);
-    print("API Response Status Code: ${response.statusCode}");
-    print("API Response Body: ${response.body}");
     if (response.statusCode == 200) {
       var jsonData = json.decode(response.body);
       if (jsonData is List) {
@@ -71,3 +67,28 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+//Mode Specific Function for get API
+// List<TodoModel> todo = [];
+// Future getDataFromApi(String apiUrl) async {
+//   try {
+//     var url = Uri.parse(apiUrl);
+//     var response = await http.get(url);
+//     print("API Response Status Code: ${response.statusCode}");
+//     print("API Response Body: ${response.body}");
+//     if (response.statusCode == 200) {
+//       List<dynamic> jsonData = json.decode(response.body);
+//       if (jsonData is List) {
+//           todo= jsonData.map((e) => TodoModel.fromJson(e)).toList();
+//         return todo;
+//       } else {
+//         throw Exception("API response format is not recognized");
+//       }
+//     } else {
+//       throw Exception("API request failed with status ${response.statusCode}");
+//     }
+//   } catch (e) {
+//     print("An error occurred: $e");
+//     throw Exception("An error occurred: $e");
+//   }
+// }
